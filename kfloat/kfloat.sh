19d8fdd63f5102169f4b7c440ece0bd8bdedf6b9
buggsboony <boonydeboon@gmail.com>
2021-01-08 22:30:32 -0400

    done, it works

--- kfloat/kfloat.sh
+++ kfloat/kfloat.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
+# console colors 2020-08-22 12:47:37
+GREEN='\033[0;32m'
+LGREEN='\033[1;32m'
+WHITE='\033[1;37m'
+YELL='\033[1;33m'
+RED='\033[0;31m'
+LRED='\033[1;31m'
+MAG='\033[0;35m'
+LMAG='\033[1;35m'
+CYAN='\033[0;36m'
+LCYAN='\033[1;36m'
+NC='\033[0m' # No Color
 
 #    ↳ USB2.0 HD UVC WebCam: USB2.0 HD           id=16   [slave  keyboard (3)]
 #    ↳ Asus WMI hotkeys                          id=19   [slave  keyboard (3)]
@@ -19,10 +30,11 @@ keyboardKill="AT Translated Set 2 keyboard"
 
 line=$(xinput | grep "$keyboardKill")
 echo "Device :"
-echo $line
+printf "${LMAG}$line\n${NC}"
 kid=$(python -c "txt = \"$line\".strip();x=txt.split(\"id=\");xid=x[1].split()[0];print(xid);")
-
+echo "xinput float $kid"
 xinput float $kid
+printf "${YELL}done.${NC}\n"
 
 #Save previous IFS
 #prevIFS=$IFS
